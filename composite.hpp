e602830a46012f45cf416c44acee91102c164d30
henock <henoctumonakiese@gmail.com>
2025-11-22 00:20:33 +0100

    creation du modele, ajout de l exe pour windows

---
+++ composite.hpp
@@ -0,0 +1,11 @@
+#include<string>
+#include<vector>
+
+void composite();
+void icone();
+void maker(int &choice);
+
+bool make_model();
+bool make_view();
+bool make_controller();
+void printer_other_choice(std::vector<std::string> option);
--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include<iostream>
+#include<string>
+#include<vector>
+#include<fstream>
+#include<cctype>
+
 
 void composite();
 void icone();
@@ -7,12 +12,10 @@ void maker(int &choice);
 bool make_model();
 bool make_view();
 bool make_controller();
-
-void printer_other_choice();
+void printer_other_choice(std::vector<std::string> option);
 
 int main()
 {
-
 
   composite();
   int choice = 0;
@@ -35,10 +38,9 @@ void composite()
      std::cout<<"[2] Create a view "<<std::endl;
      std::cout<<"[3] Create a controlloler" <<std::endl;
      std::cout<<"[4] Other ...."<<std::endl;
-}
+} 
 
-void icone()
-{
+void icone(){
 
           std::cout <<
 R"(  ____ ___  __  __ ____   ___  ____ ___ _____ _____ 
@@ -73,14 +75,45 @@ void maker(int &choice)
        break;
 
        case 4:
-           printer_other_choice();
+           printer_other_choice({
+            "Create a MVC architecture",
+            "Create a MVVM architecture",
+            "Create a MCCM architecture for api",
+            "Create a React JS project with MCCM architecture for api"
+          });
        break;
     }
 }
 
 bool make_model()
 {
-  return true;
+   std::string modelName;
+   std::string modelFile;
+
+   std::cout<<"Enter a Model name  >> ";
+   std::cin>>modelName;
+
+   if (!modelName.empty()) {
+    modelName[0] = static_cast<char>(
+        std::toupper(static_cast<unsigned char>(modelName[0]))
+    );
+   }
+
+   modelFile = modelName +".php";
+   std::ofstream model(modelFile), fichier("Model.php");
+
+   if(fichier.is_open()){
+     model<<"<?php "<<std::endl;
+     model<<"class "<<modelName<<" extends Model{\n"<<std::endl;
+     model<<"}\n"<<std::endl;
+     model.close();
+     fichier.close();
+     return true;
+   }
+   else{
+    return false;
+   }
+
 }
 
 bool make_view()
@@ -92,13 +125,13 @@ bool make_controller()
   return true;
 }
 
-void printer_other_choice()
+void printer_other_choice(std::vector<std::string> option)
 {
+  size_t length =  option.size(); int increment = 0;
 
-
-  std::cout<<"[1] Create a MVC architecture"<<std::endl;
-  std::cout<<"[2] Create a MVVM architecture"<<std::endl;
-  std::cout<<"[3] Create a MCCM architecture for api"<<std::endl;
-  std::cout<<"[4] Create a React JS project with MCCM architecture for api"<<std::endl;
-
+  for(int i ; i < length ; i++ ){
+     increment = i + 1;
+     std::cout<<'['<< increment<<']' <<option[i] <<std::endl;
+  }
+  
 }
